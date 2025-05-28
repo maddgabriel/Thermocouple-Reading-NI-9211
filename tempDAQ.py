d3b5fbc37ebec863d0e6e0a178480df85204c0f0
Gabriel Madeira <88108792+maddgabriel@users.noreply.github.com>
2025-05-28 16:29:02 -0300

    Add files via upload

---
+++ tempDAQ.py
@@ -0,0 +1,37 @@
+# Simple code for temperature measurement using NI-9211 thermocouple module.
+# -----------------------------------------
+# Author: Gabriel Madeira
+# Date: 2025-05-28
+# -----------------------------------------
+# This code requires the nidaqmx library. Install it using pip install nidaqmx.
+# Make sure your Operating System is compatible with the NI-DAQmx driver required by the hardware.
+# [important] Win 11 requires NIDAQmx 2022 Q3 for running NI-9211.
+# -----------------------------------------
+
+import nidaqmx as ni
+
+with ni.Task() as task:
+    task.ai_channels.add_ai_thrmcpl_chan("cDAQ1Mod2/ai3",                                   # Find this channel in Measurement & Automation Explorer (MAX)
+                                         units=ni.constants.TemperatureUnits.DEG_C,        
+                                         thermocouple_type=ni.constants.ThermocoupleType.K, 
+                                         cjc_source=ni.constants.CJCSource.BUILT_IN)        
+    
+    task.timing.cfg_samp_clk_timing(rate=25,                                               # Adjustable Sampling rate in Hz  
+                                     active_edge=ni.constants.Edge.RISING,                  
+                                     sample_mode=ni.constants.AcquisitionType.CONTINUOUS)   # Continuous sampling mode based on sample rate and samples per channel
+
+    task.start()
+
+    while True:
+        temperature = task.read(number_of_samples_per_channel=5)     # Read 5 samples from the Aquistion Task (adjustable) and saves on an array
+        print(f"Temperature: {temperature[0]} ºC")                   # Print a sample of the temperature reading
+
+        """
+        Note: reducing the number of prints per second without losing data can be done by adjusting the number of samples read.
+
+        temperature = task.read(number_of_samples_per_channel=125)
+        print(f"Temperature: {temperature[0]} ºC")
+
+        This will read 125 samples, so a print occurs every 5 seconds.
+        """
+# The code above will run indefinitely, reading and printing 5 times per second.
